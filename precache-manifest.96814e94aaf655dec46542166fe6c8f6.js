491fa4eadc24f3b4b0194ce21ecc3d951b462eeb
Maxim Bondar <maxim.bondarr@gmail.com>
2021-02-23 20:23:54 +0200

    Updates

---
+++ precache-manifest.96814e94aaf655dec46542166fe6c8f6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cae30666ad9151bb4b5e6633285cf2c",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "c6e046f4f252fa14ed31",
+    "url": "/social-network/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "c6e046f4f252fa14ed31",
+    "url": "/social-network/static/js/2.231447f2.chunk.js"
+  },
+  {
+    "revision": "270f03428ae26821b78e675e123c9c1b",
+    "url": "/social-network/static/js/2.231447f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a40ab736f0678d5096a",
+    "url": "/social-network/static/js/main.d79853cf.chunk.js"
+  },
+  {
+    "revision": "c2a5cf9119419fafa5ca",
+    "url": "/social-network/static/js/runtime-main.31379636.js"
+  },
+  {
+    "revision": "d15c0f428f55edcea4386131c74497df",
+    "url": "/social-network/static/media/arrow-left.d15c0f42.svg"
+  },
+  {
+    "revision": "7011d9f83373fa5367faea2893ccbed9",
+    "url": "/social-network/static/media/arrow-right.7011d9f8.svg"
+  },
+  {
+    "revision": "1e88ce65f6fe73a0e122f4fea65d2aed",
+    "url": "/social-network/static/media/close.1e88ce65.svg"
+  },
+  {
+    "revision": "584c3b1b9d73104ab4941824a187e5a6",
+    "url": "/social-network/static/media/delete.584c3b1b.svg"
+  },
+  {
+    "revision": "d2efc587b250e5c2acaa3e7778b65106",
+    "url": "/social-network/static/media/eye-close.d2efc587.svg"
+  },
+  {
+    "revision": "46ee1c114218db8af0925c6b561e275a",
+    "url": "/social-network/static/media/eye-open.46ee1c11.svg"
+  },
+  {
+    "revision": "a9692538060ddd57b716ed3521b0956f",
+    "url": "/social-network/static/media/hero-dark-mode.a9692538.jpg"
+  },
+  {
+    "revision": "c94559a47819b386d0909933218c404f",
+    "url": "/social-network/static/media/hero.c94559a4.jpg"
+  },
+  {
+    "revision": "bbea47de76c91dcf82a5149d20c60afd",
+    "url": "/social-network/static/media/like.bbea47de.svg"
+  },
+  {
+    "revision": "c88ceed22f4d8dd24589cf47b5bc62de",
+    "url": "/social-network/static/media/logo-dark-mode.c88ceed2.png"
+  },
+  {
+    "revision": "443c9dd0d08b55d6e6246ea7157c50bc",
+    "url": "/social-network/static/media/logo.443c9dd0.png"
+  },
+  {
+    "revision": "f30c5f00454133fbe8a432ea4d53bc21",
+    "url": "/social-network/static/media/spinner.f30c5f00.svg"
+  }
+]);
